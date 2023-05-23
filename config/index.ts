7e1c8817edceee96d19df4ff11d88b779baf24e9
rabieeali <alirbi.webdev@gmail.com>
2023-05-23 14:01:14 +0330

    read excel files with nodeJs, save them in Database or save them somewhere else in your system

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,11 @@
-enum Mongo_Credentials {
-    Mongo_URI = 'mongodb+srv://ali1234:ali1234@clustersavefiledata.yqgpot6.mongodb.net/',
-    DB_Name = 'files',
-    Collection_Name = 'excels'
-}
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const Mongo_Credentials = {
+    Mongo_URI: `mongodb+srv://${process.env.MONGO_USER_NAME}:${process.env.MONGO_PASSWORD}@clustersavefiledata.yqgpot6.mongodb.net/`,
+    DB_Name: 'files',
+    Collection_Name: 'excels'
+};
 
 export { Mongo_Credentials }
